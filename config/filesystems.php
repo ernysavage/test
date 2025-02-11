a76fe9afe5fb7ec0b479ef44c9bebfb780cbd36f
Фишер Эрнест Сергеевич <e.fisher@dev.tech>
2025-02-11 16:34:53 +0500

    Исправлен коммит: удалены локальные файлы и vendor

--- config/filesystems.php
+++ config/filesystems.php
@@ -2,32 +2,10 @@
 
 return [
 
-    /*
-    |--------------------------------------------------------------------------
-    | Default Filesystem Disk
-    |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application for file storage.
-    |
-    */
 
     'default' => env('FILESYSTEM_DISK', 'local'),
 
-    /*
-    |--------------------------------------------------------------------------
-    | Filesystem Disks
-    |--------------------------------------------------------------------------
-    |
-    | Below you may configure as many filesystem disks as necessary, and you
-    | may even configure multiple disks for the same driver. Examples for
-    | most supported storage drivers are configured here for reference.
-    |
-    | Supported drivers: "local", "ftp", "sftp", "s3"
-    |
-    */
-
+    
     'disks' => [
 
         'local' => [
@@ -37,9 +15,9 @@ return [
             'throw' => false,
         ],
 
-       /* 'public' => [
+        'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('app/public'),  
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
@@ -64,6 +42,6 @@ return [
     'links' => [
         public_path('storage') => storage_path('app/public'),
     ],
-*/
-    ]
+
+    
 ];
